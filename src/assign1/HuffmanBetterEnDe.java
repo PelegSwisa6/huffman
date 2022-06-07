fcb64371cda4ebf56dc0bc2a3dbeaaad4d961760
Peleg Swisa <107035268+PelegSwisa6@users.noreply.github.com>
2022-06-07 13:19:48 +0300

    Add files via upload

---
+++ src/assign1/HuffmanBetterEnDe.java
@@ -0,0 +1,23 @@
+package assign1;
+
+/**
+ * Assignment 1
+ * Submitted by: 
+ * Student 1.Ron Amar 	ID# 318541745
+ * Student 2.Peleg Swisa ID# 207726812
+ * Student 2.Rotem Mazuz ID# 206511321
+ */
+
+//Uncomment if you wish to use FileOutputStream and FileInputStream for file access.
+//import java.io.FileOutputStream;
+//import java.io.FileInputStream;
+
+public class HuffmanBetterEnDe extends HuffmanEncoderDecoder
+{
+
+	public HuffmanBetterEnDe()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
